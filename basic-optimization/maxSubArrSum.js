ffdb7938cd5d6446c3ed78d2e37dbc345393c02d
HollowKnight-07 <216045749+HollowKnight-07@users.noreply.github.com>
2026-07-01 10:08:31 +0000

    solve by kadane's algorithm

---
+++ basic-optimization/maxSubArrSum.js
@@ -0,0 +1,14 @@
+const maxSubArrSum=(a)=>{
+let sum=0
+let max=-Infinity
+for(const nums of a){
+  sum+=nums
+  max=Math.max(max,sum)
+  if(sum<0){
+    sum=0
+}
+}
+return max
+}
+const arr=[ -2, 1, -3, 4, -1, 2, 1, -5, 4 ]
+console.log(maxSubArrSum(arr))
